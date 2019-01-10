154194735021f4bf437619c24806035a7078b6c4
lucifer911 <balagmj@gmail.com>
2019-01-09 21:49:49 -0500

    Update 1test.sh

--- 1test.sh
+++ 1test.sh
@@ -1,25 +1,8 @@
 
 #!/bin/sh
 #Version 0.1.1.3
-mkdir a
-touch a/a.conf
-read -p "Enter server names separated by 'space' : " input
+echo "Enter_privatekey  :"
 
-for i in ${input[@]}
-do
-echo ""
-echo "User entered value :"$i    # or do whatever with individual element of the array
-echo ""
-done
+read pivkey
 
-echo "rpcuser=${_rpcUserName}
-rpcpassword=${_rpcPassword}
-rpcallowip=127.0.0.1
-listen=1
-server=1
-daemon=1
-masternode=1
-masternodeprivkey=${_nodePrivateKey}
-" > a.conf
-
-cat a/a.conf
+echo "Enter_privatekey  : $pivkey"

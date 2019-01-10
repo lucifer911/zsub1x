2d512f3a53c82e44e161a384d2722510d27c1b8b
lucifer911 <balagmj@gmail.com>
2019-01-09 21:20:00 -0500

    Update 1test.sh

--- 1test.sh
+++ 1test.sh
@@ -1,44 +1,68 @@
-#!/bin/bash
-# install.sh
-# Installs smartnode on Ubuntu 16.04 LTS x64
-# ATTENTION: The anti-ddos part will disable http, https and dns ports.
+#!/bin/sh
+#Version 0.1.1.3
+#Info: Installs zsub1x daemon, Masternode based on privkey.
+#zsub1x
+#Tested OS: Ubuntu 16.04
+#All the credit to Chaobunga
 
-echo "enter your private key"
-echo "Type the new message that you want to enter, followed by [ENTER]:"
-read privkey
-_nodePrivateKey=$(privkey)
+
+noflags() {
+	echo "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"
+    echo "Usage: install-sub1"
+    echo "Example: install-sub1"
+    echo "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"
+    exit 1
+}
+
+message() {
+	echo "╒════════════════════════════════════════════════════════════════════════════════>>"
+	echo "| $1"
+	echo "╘════════════════════════════════════════════<<<"
+}
+
+error() {
+	message "An error occured, you must fix it to continue!"
+	exit 1
+}
 
 # Get a new privatekey by going to console >> debug and typing smartnode genkey
-# printf "Masternode GenKey: "
-# read _nodePrivateKey
+pvkey() { #TODO: get pvkey
+	message "Get a new privatekey by going to console >> debug and typing smartnode genkey"
+	printf "Masternode GenKey: "
+	read _nodePrivateKey
+}
 
-# The RPC node will only accept connections from your localhost
-_rpcUserName=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 12 ; echo '')
+createconf() {
+	#TODO: Can check for flag and skip this
+	#TODO: Random generate the user and password
 
-# Choose a random and secure password for the RPC
-_rpcPassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
+	message "Creating chaincoin.conf..."
+	CONFDIR=~/.zsub1x
+	CONFILE=$CONFDIR/zsub1x.conf
+	if [ ! -d "$CONFDIR" ]; then mkdir $CONFDIR; fi
+	if [ $? -ne 0 ]; then error; fi
+	mnip=$(curl -s https://api.ipify.org)
+	rpcuser=$(date +%s | sha256sum | base64 | head -c 10 ; echo)
+	rpcpass=$(openssl rand -base64 32)
+	printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "rpcport=1331" "listen=1" "server=1" "daemon=1" "maxconnections=256" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=${_nodePrivateKey}" "masternodeaddr=$mnip:5721" "addnode=sub1x.seeds.mn.zone" > $CONFILE
+       
+        zsub1xd
+        message "Wait 10 seconds for daemon to load..."
+        sleep 20s
+        MNPRIVKEY=$(zsub1x-cli masternode genkey)
+		zsub1x-cli stop
+		message "wait 10 seconds for deamon to stop..."
+        sleep 10s
+		sudo rm $CONFILE
+		message "Updating chaincoin.conf..."
+        printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "rpcport=1331" "listen=1" "server=1" "daemon=1" "maxconnections=256" "externalip=$mnip" "bind=$mnip" "masternode=1" "masternodeprivkey=${_nodePrivateKey}" "masternodeaddr=$mnip:5721" "addnode=sub1x.seeds.mn.zone" > $CONFILE
 
-# Get the IP address of your vps which will be hosting the smartnode
-_nodeIpAddress=$(ip route get 1 | awk '{print $NF;exit}')
+}
 
-# Make a new directory for chaincoin daemon
-mkdir ~/.1/
-touch ~/.1/1.conf
+install() {
+	pvkey
+  createconf
+}
 
-# Change the directory to ~/.chaincoin
-cd ~/.1/
-
-# Create the initial chaincoin.conf file
-echo "rpcuser=${_rpcUserName}
-rpcpassword=${_rpcPassword}
-rpcallowip=127.0.0.1
-listen=1
-server=1
-daemon=1
-masternode=1
-masternodeprivkey=${_nodePrivateKey}
-" > 1.conf
-
-# Create a directory for chcnode's cronjobs and the anti-ddos script
-cd
-cd .chaincoincore
+#main
+#default to --without-gui

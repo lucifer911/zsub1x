0129540d3669e9d954eaf3083aa3b877e287af22
lobo <lobo-mf@hotmail.com>
2019-04-08 14:29:12 -0300

    Update install-sub.sh

--- install-sub.sh
+++ install-sub.sh
@@ -1,122 +1,339 @@
-#!/bin/bash
-#Version 0.1.1.3
-#Info: Installs zsub1x daemon, Masternode based on privkey.
-#zsub1x 1.4
-#Tested OS: Ubuntu 16.04
-#All the credit to Chaobunga
-MNPRIVKEY="default"
-
-noflags() {
-	echo "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"
-    echo "Usage: install-sub1"
-    echo "Example: install-sub1"
-    echo "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"
-    exit 1
+#/bin/bash
+cd ~
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+IP4COUNT=$(find /root/.zsub1x_* -maxdepth 0 -type d | wc -l)
+DELETED="$(cat /root/bin/deletedzsub1x | wc -l)"
+ALIASES="$(find /root/.zsub1x_* -maxdepth 0 -type d | cut -c22-)"
+face="$(lshw -C network | grep "logical name:" | sed -e 's/logical name:/logical name: /g' | awk '{print $3}' | head -n1)"
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
+fi
+function configure_systemd() {
+  cat << EOF > /etc/systemd/system/zsub1xd$ALIAS.service
+[Unit]
+Description=zsub1xd$ALIAS service
+After=network.target
+ [Service]
+User=root
+Group=root
+Type=forking
+#PIDFile=/root/.zsub1x_$ALIAS/zsub1xd.pid
+ExecStart=/root/bin/zsub1xd_$ALIAS.sh
+ExecStop=/root/bin/zsub1x-cli_$ALIAS.sh stop
+Restart=always
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=10s
+StartLimitInterval=120s
+StartLimitBurst=5
+ [Install]
+WantedBy=multi-user.target
+EOF
+  systemctl daemon-reload
+  sleep 6
+  crontab -l > cron$ALIAS
+  echo "@reboot systemctl start zsub1xd$ALIAS" >> cron$ALIAS
+  crontab cron$ALIAS
+  rm cron$ALIAS
+  systemctl start zsub1xd$ALIAS.service
 }
+IP4=$(curl -s4 api.ipify.org)
+perl -i -ne 'print if ! $a{$_}++' /etc/network/interfaces
+if [ ! -f "/usr/local/bin/zsub1xd" ]; then
+ DOSETUP="y"
+else
+ DOSETUP="n"
+fi
+clear
+echo "1 - Create new nodes"
+echo "2 - Remove an existing node"
+echo "3 - List aliases"
+echo "4 - Check for node errors"
+echo "What would you like to do?"
+read DO
+echo ""
 
-message() {
-	echo "╒════════════════════════════════════════════════════════════════════════════════>>"
-	echo "| $1"
-	echo "╘════════════════════════════════════════════<<<"
-}
+if [ $DO = "4" ]
+then
+echo $ALIASES > temp1
+cat temp1 | grep -o '[^ |]*' > temp2
+CN="$(cat temp2 | wc -l)"
+rm temp1
+let LOOP=0
+while [  $LOOP -lt $CN ]; do
+LOOP=$((LOOP+1))
+CURRENT="$(sed -n "${LOOP}p" temp2)"
+echo -e "${GREEN}${CURRENT}${NC}:"
+sh /root/bin/zsub1x-cli_${CURRENT}.sh masternode status | grep "message"
+OFFSET="$(sh /root/bin/zsub1x-cli_${CURRENT}.sh getinfo | grep "timeoffset")"
+OFF1=${OFFSET:(-2)}
+OFF=${OFF1:0:1}
+if [ $OFF = "1" ]
+then
+echo "$OFFSET" 
+fi
+done
+rm temp2
+fi
+if [ $DO = "3" ]
+then
+echo -e "${GREEN}${ALIASES}${NC}"
+echo ""
+echo "1 - Create new nodes"
+echo "2 - Remove an existing node"
+echo "What would you like to do?"
+read DO
+echo ""
+fi
+if [ $DO = "2" ]
+then
+perl -i -ne 'print if ! $a{$_}++' /etc/monit/monitrc >/dev/null 2>&1
+echo "Input the alias of the node that you want to delete"
+read ALIASD
+echo ""
+echo -e "${GREEN}Deleting ${ALIASD}${NC}. Please wait."
+## Removing service
+systemctl stop zsub1xd$ALIASD >/dev/null 2>&1
+systemctl disable zsub1xd$ALIASD >/dev/null 2>&1
+rm /etc/systemd/system/zsub1xd${ALIASD}.service >/dev/null 2>&1
+systemctl daemon-reload >/dev/null 2>&1
+systemctl reset-failed >/dev/null 2>&1
+rm /root/.zsub1x_$ALIASD -r >/dev/null 2>&1
+sed -i "/${ALIASD}/d" .bashrc
+crontab -l -u root | grep -v zsub1xd$ALIASD | crontab -u root - >/dev/null 2>&1
+source .bashrc
+echo "1" >> /root/bin/deletedzsub1x
+echo -e "${ALIASD} Successfully deleted."
+fi
+if [ $DO = "1" ]
+then
+echo "1 - Easy mode"
+echo "2 - Expert mode"
+echo "Please select a option:"
+read EE
+echo ""
+if [ $EE = "1" ] 
+then
+MAXC="64"
+fi
+if [ $EE = "2" ] 
+then
+echo ""
+echo "Enter max connections value"
+read MAXC
+fi
+if [ $DOSETUP = "y" ]
+then
+  echo -e "Installing ${GREEN}zsub1x dependencies${NC}. Please wait."
+  apt-get update 
+  apt-get -y upgrade
+  apt-get -y dist-upgrade
+  apt-get update
+  apt-get install -y zip unzip bc curl nano lshw gawk
+  echo -e "${RED}Creating swap. This may take a while.${NC}"
+  dd if=/dev/zero of=/var/swap.img bs=2048 count=1M
+  chmod 600 /var/swap.img
+  mkswap /var/swap.img 
+  swapon /var/swap.img 
+  free -m
+  echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+  cd /root 
+  thr="$(nproc)"
+  export LC_CTYPE=en_US.UTF-8
+  export LC_ALL=en_US.UTF-8
+  apt install software-properties-common -y
+  add-apt-repository universe -y
+  apt update
+  apt install -y libdb++-dev git zip automake software-properties-common unzip build-essential libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev devscripts libsodium-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libminiupnpc-dev qt5-default gcc-5 g++-5 --auto-remove
+  apt update
+  apt install libssl1.0-dev -y
+  apt install libzmq3-dev -y --auto-remove
+  git clone https://github.com/SuB1X-Coin/zSub1x.git
+  cd zSub1x
+  chmod +x */*/*
+  update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100
+  update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 100
+  ./autogen.sh
+  ./configure --with-incompatible-bdb --disable-tests
+  make -j $thr
+  make install
+  cd ..
+  rm zSub1x -r
+  mkdir /root/bin
+  apt-get install -y ufw 
+  ufw allow ssh/tcp 
+  ufw limit ssh/tcp 
+  ufw logging on
+  echo "y" | ufw enable 
+  ufw allow 7250
+  mkdir -p ~/bin 
+  echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
+  source ~/.bashrc
+  echo ""
+  cd
+  sysctl vm.swappiness=10
+  sysctl vm.vfs_cache_pressure=200
+  echo 'vm.swappiness=10' | tee -a /etc/sysctl.conf
+  echo 'vm.vfs_cache_pressure=200' | tee -a /etc/sysctl.conf
+fi
+gateway1=$(/sbin/route -A inet6 | grep -v ^fe80 | grep -v ^ff00 | grep -w "$face")
+gateway2=${gateway1:0:26}
+gateway3="$(echo -e "${gateway2}" | tr -d '[:space:]')"
+if [[ $gateway3 = *"128"* ]]; then
+  gateway=${gateway3::-5}
+fi
+if [[ $gateway3 = *"64"* ]]; then
+  gateway=${gateway3::-3}
+fi
+MASK="/64"
+echo -e "zsub1x nodes currently installed: ${GREEN}${IP4COUNT}${NC}, zsub1x nodes previously Deleted: ${GREEN}${DELETED}${NC}"
+echo ""
+if [ $IP4COUNT = "0" ] 
+then
+echo -e "${RED}First node must be ipv4.${NC}"
+let COUNTER=0
+PORT=5721
+RPCPORTT=57210
+RPCPORT=$(($RPCPORTT+$COUNTER))
+  echo ""
+  echo "Enter alias for first node"
+  read ALIAS
+  CONF_DIR=~/.zsub1x_$ALIAS
+  echo ""
+  echo "Enter masternode private key for node $ALIAS"
+  read PRIVKEY
+  if [ $EE = "2" ] 
+	then
+	echo ""
+	echo "Enter port for $ALIAS"
+	read PORTD
+  fi
+  mkdir ~/.zsub1x_$ALIAS
+  echo '#!/bin/bash' > ~/bin/zsub1xd_$ALIAS.sh
+  echo "zsub1xd -daemon -conf=$CONF_DIR/zsub1x.conf -datadir=$CONF_DIR "'$*' >> ~/bin/zsub1xd_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/zsub1x-cli_$ALIAS.sh
+  echo "zsub1x-cli -conf=$CONF_DIR/zsub1x.conf -datadir=$CONF_DIR "'$*' >> ~/bin/zsub1x-cli_$ALIAS.sh
+  chmod 755 ~/bin/zsub1x*.sh
+  mkdir -p $CONF_DIR
+  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> zsub1x.conf_TEMP
+  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> zsub1x.conf_TEMP
+  echo "rpcallowip=127.0.0.1" >> zsub1x.conf_TEMP
+  echo "rpcport=$RPCPORT" >> zsub1x.conf_TEMP
+  echo "listen=1" >> zsub1x.conf_TEMP
+  echo "server=1" >> zsub1x.conf_TEMP
+  echo "daemon=1" >> zsub1x.conf_TEMP
+  echo "logtimestamps=1" >> zsub1x.conf_TEMP
+  echo "maxconnections=$MAXC" >> zsub1x.conf_TEMP
+  echo "masternode=1" >> zsub1x.conf_TEMP
+  echo "dbcache=20" >> zsub1x.conf_TEMP
+  echo "maxorphantx=5" >> zsub1x.conf_TEMP
+  echo "maxmempool=100" >> zsub1x.conf_TEMP
+  echo "" >> zsub1x.conf_TEMP
+  echo "" >> zsub1x.conf_TEMP
+  echo "bind=$IP4:$PORT" >> zsub1x.conf_TEMP
+  echo "externalip=$IP4" >> zsub1x.conf_TEMP
+  echo "masternodeaddr=$IP4:$PORT" >> zsub1x.conf_TEMP
+  echo "masternodeprivkey=$PRIVKEY" >> zsub1x.conf_TEMP
+  
 
-error() {
-	message "An error occured, you must fix it to continue!"
-	exit 1
-}
-
-
-privatekey() { #TODO: add error detection
-	message "Get a new privatekey by going to console >> debug and typing smartnode genkey"
-	printf "Masternode GenKey: "
-	read MNPRIVKEY
-	echo "privatekey: $MNPRIVKEY"
-	#read -p "enter you priv key" MNPRIVKEY 
-}
-	
-prepdependencies() { #TODO: add error detection
-	message "Installing dependencies..."
-	sudo apt-get update
-	sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-	sudo apt-get install automake libdb++-dev build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libminiupnpc-dev git software-properties-common python-software-properties g++ bsdmainutils libevent-dev -y
-	sudo add-apt-repository ppa:bitcoin/bitcoin -y
-	sudo apt-get install unzip -y
-	sudo apt-get update
-	sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-}
-	
-createswap() { #TODO: add error detection
-	message "Creating 2GB temporary swap file...this may take a few minutes..."
-	cd /var
-	sudo touch swap.img
-	sudo chmod 600 swap.img
-	sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-	sudo mkswap /var/swap.img
-	sudo swapon /var/swap.img
-	sudo free
-	sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
-	cd
-	cd
-}
-
-
-installwallet() { #TODO: add error detection
-	message "Downloading the wallet & moving files..."
-	sudo wget https://github.com/SuB1X-Coin/zSub1x/releases/download/1.4.0/zSub1x_1.4.0_Linux_daemon.zip
-	sudo unzip zSub1x_1.4.0_Linux_daemon.zip
-	sudo chmod +x zsub1xd
-	sudo chmod +x zsub1x-cli
-	sudo mv zsub1x-cli /usr/local/bin/
-	sudo mv zsub1xd /usr/local/bin/
-	sudo rm -r zSub1x_1.4.0_Linux_daemon.zip
-}	
-
-createconf() {
-	#TODO: Can check for flag and skip this
-	#TODO: Random generate the user and password
-
-	message "Creating chaincoin.conf..."
-	#MNPRIVKEY="88Rf6fN7erDou9KqzChntcRE6deh6KrjTNfv2hfrM2VHKkWyxiu"
-	#echo "$MNPRIVKEY"
-	CONFDIR=~/.zsub1x
-	CONFILE=$CONFDIR/zsub1x.conf
-	if [ ! -d "$CONFDIR" ]; then mkdir $CONFDIR; fi
-	if [ $? -ne 0 ]; then error; fi
-	mnip=$(curl -s https://api.ipify.org)
-	rpcuser=$(date +%s | sha256sum | base64 | head -c 10 ; echo)
-	rpcpass=$(openssl rand -base64 32)
-	printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "rpcport=1331" "listen=1" "server=1" "daemon=1" "maxconnections=256" "bind=$mnip" "externalip=$mnip:5721" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "addnode=sub1x.seeds.mn.zone" "addnode=62.75.163.187" > $CONFILE
-       
-        zsub1xd
-        message "Wait 20 seconds for daemon to load..."
-        sleep 20s
-        MNPRIVKEY="$MNPRIVKEY"
-		zsub1x-cli stop
-		message "wait 10 seconds for deamon to stop..."
-        sleep 10s
-		sudo rm $CONFILE
-		message "Updating chaincoin.conf..."
-        printf "%s\n" "rpcuser=$rpcuser" "rpcpassword=$rpcpass" "rpcallowip=127.0.0.1" "rpcport=1331" "listen=1" "server=1" "daemon=1" "maxconnections=256" "bind=$mnip" "externalip=$mnip:5721" "masternode=1" "masternodeprivkey=$MNPRIVKEY" "addnode=sub1x.seeds.mn.zone" "addnode=62.75.163.187" > $CONFILE
-
-}
-
-success() {
-	zsub1xd
-	message "SUCCESS! Your zsub1x has started. Masternode.conf setting below..."
-	message "MN $mnip:5721 $MNPRIVKEY TXHASH INDEX"
-	exit 0	
-}
-
-install() {
-	privatekey
-	prepdependencies
-	createswap
-	installwallet
-	createconf
-	success
-}
-
-#main
-#default to --without-gui
-install --without-gui
-zsub1xd
+  mv zsub1x.conf_TEMP $CONF_DIR/zsub1x.conf
+  echo ""
+  echo -e "Your ip is ${GREEN}$IP4:$PORT${NC}"
+	echo "alias ${ALIAS}_status=\"zsub1x-cli -datadir=/root/.zsub1x_${ALIAS} masternode status\"" >> .bashrc
+	echo "alias ${ALIAS}_stop=\"systemctl stop zsub1xd$ALIAS\"" >> .bashrc
+	echo "alias ${ALIAS}_start=\"systemctl start zsub1xd$ALIAS\""  >> .bashrc
+	echo "alias ${ALIAS}_config=\"nano /root/.zsub1x_${ALIAS}/zsub1x.conf\""  >> .bashrc
+	echo "alias ${ALIAS}_getinfo=\"zsub1x-cli -datadir=/root/.zsub1x_${ALIAS} getinfo\"" >> .bashrc
+    	echo "alias ${ALIAS}_getpeerinfo=\"zsub1x-cli -datadir=/root/.zsub1x_${ALIAS} getpeerinfo\"" >> .bashrc
+	echo "alias ${ALIAS}_resync=\"/root/bin/zsub1xd_${ALIAS}.sh -resync\"" >> .bashrc
+	echo "alias ${ALIAS}_reindex=\"/root/bin/zsub1xd_${ALIAS}.sh -reindex\"" >> .bashrc
+	echo "alias ${ALIAS}_restart=\"systemctl restart zsub1xd$ALIAS\""  >> .bashrc
+	## Config Systemctl
+	configure_systemd
+fi
+if [ $IP4COUNT != "0" ] 
+then
+echo "How many ipv6 nodes do you want to install on this server?"
+read MNCOUNT
+let MNCOUNT=MNCOUNT+1
+let MNCOUNT=MNCOUNT+IP4COUNT
+let MNCOUNT=MNCOUNT+DELETED
+let COUNTER=1
+let COUNTER=COUNTER+IP4COUNT
+let COUNTER=COUNTER+DELETED
+while [  $COUNTER -lt $MNCOUNT ]; do
+ PORT=5721
+ RPCPORTT=57210
+ RPCPORT=$(($RPCPORTT+$COUNTER))
+  echo ""
+  echo "Enter alias for new node"
+  read ALIAS
+  CONF_DIR=~/.zsub1x_$ALIAS
+  echo ""
+  echo "Enter masternode private key for node $ALIAS"
+  read PRIVKEY
+  echo "up /sbin/ip -6 addr add ${gateway}$COUNTER$MASK dev $face # $ALIAS" >> /etc/network/interfaces
+  /sbin/ip -6 addr add ${gateway}$COUNTER$MASK dev $face
+  mkdir ~/.zsub1x_$ALIAS
+  unzip Bootstrap.zip -d ~/.zsub1x_$ALIAS >/dev/null 2>&1
+  echo '#!/bin/bash' > ~/bin/zsub1xd_$ALIAS.sh
+  echo "zsub1xd -daemon -conf=$CONF_DIR/zsub1x.conf -datadir=$CONF_DIR "'$*' >> ~/bin/zsub1xd_$ALIAS.sh
+  echo '#!/bin/bash' > ~/bin/zsub1x-cli_$ALIAS.sh
+  echo "zsub1x-cli -conf=$CONF_DIR/zsub1x.conf -datadir=$CONF_DIR "'$*' >> ~/bin/zsub1x-cli_$ALIAS.sh
+  chmod 755 ~/bin/zsub1x*.sh
+  mkdir -p $CONF_DIR
+  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> zsub1x.conf_TEMP
+  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> zsub1x.conf_TEMP
+  echo "rpcallowip=127.0.0.1" >> zsub1x.conf_TEMP
+  echo "rpcport=$RPCPORT" >> zsub1x.conf_TEMP
+  echo "listen=1" >> zsub1x.conf_TEMP
+  echo "server=1" >> zsub1x.conf_TEMP
+  echo "daemon=1" >> zsub1x.conf_TEMP
+  echo "logtimestamps=1" >> zsub1x.conf_TEMP
+  echo "maxconnections=$MAXC" >> zsub1x.conf_TEMP
+  echo "masternode=1" >> zsub1x.conf_TEMP
+  echo "dbcache=20" >> zsub1x.conf_TEMP
+  echo "maxorphantx=5" >> zsub1x.conf_TEMP
+  echo "maxmempool=100" >> zsub1x.conf_TEMP
+  echo "bind=[${gateway}$COUNTER]:$PORT" >> zsub1x.conf_TEMP
+  echo "externalip=[${gateway}$COUNTER]" >> zsub1x.conf_TEMP
+  echo "masternodeaddr=[${gateway}$COUNTER]:$PORT" >> zsub1x.conf_TEMP
+  echo "masternodeprivkey=$PRIVKEY" >> zsub1x.conf_TEMP
+  mv zsub1x.conf_TEMP $CONF_DIR/zsub1x.conf
+  echo ""
+  echo -e "Your ip is ${GREEN}[${gateway}$COUNTER]:$PORT${NC}"
+	echo "alias ${ALIAS}_status=\"zsub1x-cli -datadir=/root/.zsub1x_${ALIAS} masternode status\"" >> .bashrc
+	echo "alias ${ALIAS}_stop=\"systemctl stop zsub1xd$ALIAS\"" >> .bashrc
+	echo "alias ${ALIAS}_start=\"systemctl start zsub1xd$ALIAS\""  >> .bashrc
+	echo "alias ${ALIAS}_config=\"nano /root/.zsub1x_${ALIAS}/zsub1x.conf\""  >> .bashrc
+	echo "alias ${ALIAS}_getinfo=\"zsub1x-cli -datadir=/root/.zsub1x_${ALIAS} getinfo\"" >> .bashrc
+        echo "alias ${ALIAS}_getpeerinfo=\"zsub1x-cli -datadir=/root/.zsub1x_${ALIAS} getpeerinfo\"" >> .bashrc
+	echo "alias ${ALIAS}_resync=\"/root/bin/zsub1xd_${ALIAS}.sh -resync\"" >> .bashrc
+	echo "alias ${ALIAS}_reindex=\"/root/bin/zsub1xd_${ALIAS}.sh -reindex\"" >> .bashrc
+	echo "alias ${ALIAS}_restart=\"systemctl restart zsub1xd$ALIAS\""  >> .bashrc
+	## Config Systemctl
+	configure_systemd
+	COUNTER=$((COUNTER+1))
+done
+fi
+echo ""
+echo "Commands:"
+echo "${ALIAS}_start"
+echo "${ALIAS}_restart"
+echo "${ALIAS}_status"
+echo "${ALIAS}_stop"
+echo "${ALIAS}_config"
+echo "${ALIAS}_getinfo"
+echo "${ALIAS}_getpeerinfo"
+echo "${ALIAS}_resync"
+echo "${ALIAS}_reindex"
+fi
+echo ""
+echo "Made by lobo"
+echo "Transcendence Address for donations: GWe4v6A6tLg9pHYEN5MoAsYLTadtefd9o6"
+echo "Bitcoin Address for donations: 1NqYjVMA5DhuLytt33HYgP5qBajeHLYn4d"
+exec bash
+exit
